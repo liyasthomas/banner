5606ffb3169cc486e97bc65a3082bbfe4cd67c52
liyasthomas <liyascthomas@gmail.com>
2019-06-04 12:30:58 +0530

    :tada: Initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const capture = document.getElementById('capture')
+const bgc = document.getElementById('bgc')
+const borderbg = document.getElementById('borderbg')
+const heading = document.getElementById('heading')
+const root = document.querySelector(':root')
+const download = () => {
+	html2canvas(capture, {
+		width: 1280,
+		height: 375,
+		x: capture.offsetLeft,
+		y: capture.offsetTop,
+		scale: 1
+	}).then(canvas => {
+		saveAs(canvas.toDataURL(), 'banner.png')
+	})
+}
+const saveAs = (uri, filename) => {
+	const link = document.createElement('a')
+	if (typeof link.download === 'string') {
+		link.href = uri
+		link.download = filename
+		document.body.appendChild(link)
+		link.click()
+		document.body.removeChild(link)
+	} else {
+		window.open(uri)
+	}
+}
+const border = ({
+	value
+}) => {
+	capture.style.borderWidth = value + `px`
+}
+const fontsize = ({
+	value
+}) => {
+	heading.style.fontSize = value + `px`
+}
+let picker1 = new Picker({
+	parent: bgc,
+	color: getComputedStyle(document.documentElement).getPropertyValue('--bg-color'),
+	popup: 'left',
+	alpha: false,
+	onChange({
+		rgbaString
+	}) {
+		root.style.setProperty('--bg-color', rgbaString)
+	}
+})
+let picker2 = new Picker({
+	parent: bfc,
+	color: getComputedStyle(document.documentElement).getPropertyValue('--font-color'),
+	popup: 'left',
+	alpha: false,
+	onChange({
+		rgbaString
+	}) {
+		root.style.setProperty('--font-color', rgbaString)
+	}
+})
+let picker3 = new Picker({
+	parent: borderbg,
+	color: getComputedStyle(document.documentElement).getPropertyValue('--borderbg-color'),
+	popup: 'left',
+	alpha: false,
+	onChange({
+		rgbaString
+	}) {
+		root.style.setProperty('--borderbg-color', rgbaString)
+	}
+})
+const random = () => {
+	root.style.setProperty('--bg-color', generateRandomHexColor())
+	root.style.setProperty('--font-color', generateRandomHexColor())
+	root.style.setProperty('--borderbg-color', generateRandomHexColor())
+}
+const generateRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`
